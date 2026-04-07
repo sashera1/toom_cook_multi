005996595887ca4dbb5f42a988542c5c4e6c2c5d
sashera1 <ukk8zb@virginia.edu>
2026-04-06 22:57:25 -0400

    begining structure

--- MassiveInteger.java
+++ MassiveInteger.java
@@ -6,11 +6,18 @@ public class MassiveInteger{
     is stored in little endian form
     a.operation(b) modifies and places the result in a,
     a = operation(b,c) creates new MassiveInteger a 
+    array of ints will always store magnitude:
+    sign as boolean flag (0 counts as positive)
     */
 
     private int[] contents;
+    private boolean positive;
 
-    
+    public MassiveInteger(String inputStr){
+
+    }
+
+
 
 
 
